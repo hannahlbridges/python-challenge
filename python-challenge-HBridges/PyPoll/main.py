f87f1fb3782aac5c70c32e5b0bfb33f641e13fb0
Hannah Bridges <hannahlbridges@gmail.com>
2020-09-15 21:55:39 -0400

    Title, Candidate Names and Total Votes scripted

--- python-challenge-HBridges/PyPoll/main.py
+++ python-challenge-HBridges/PyPoll/main.py
@@ -4,9 +4,24 @@ import os
 print('Election Results')
 print('-----------------------------------------')
 
-resultspath = '/Resources/election_data.csv'
+resultspath = 'Resources/election_data.csv'
+
+
 
 with open(resultspath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
+    candidates = []
+    for vote in csvreader:
+        candidates.append(vote[2]) if vote[2] not in candidates else candidates
+        
+with open(resultspath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)        
+    total_votes = sum(1 for row in csvreader)
+
+print(f'{candidates[0]}:')
+print(f'{candidates[1]}:')
+print(f'{candidates[2]}:')
+print(f'{candidates[3]}:')
     
